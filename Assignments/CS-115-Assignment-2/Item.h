40fb8c8dbcee774320eaa9390679c726e3eef13b
Owen Monus <owenmonus@icloud.com>
2022-09-21 16:33:07 -0600

    Added Interface Description Templates

--- Assignments/CS-115-Assignment-2/Item.h
+++ Assignments/CS-115-Assignment-2/Item.h
@@ -21,18 +21,137 @@ struct Item {
     string itemDescription;
 };
 
+//
+//  itemInit
+//
+//  Purpose: PUT TEXT HERE
+//  Parameter(s):
+//    <1> item: 
+//    <2> id1:
+//    <3> row1:
+//    <4> column1:
+//    <5> points1:
+//    <6> world_description1:
+//    <7> inventory_description1:
+//  Precondition(s): PUT TEXT HERE
+//  Returns: N/A
+//  Side Effect: PUT TEXT HERE
+//
 void itemInit (Item& item, char id1, 
                int row1, int column1, 
                int points1,
                const string& world_description1,
                const string& inventory_description1);
+
+//
+//  itemDebugPrint
+//
+//  Purpose: PUT TEXT HERE
+//  Parameter(s):
+//    <1> item: 
+//  Precondition(s): PUT TEXT HERE
+//  Returns: N/A
+//  Side Effect: PUT TEXT HERE
+//
 void itemDebugPrint (const Item& item);
+
+//
+//  itemGetId
+//
+//  Purpose: PUT TEXT HERE
+//  Parameter(s):
+//    <1> item: 
+//  Precondition(s): PUT TEXT HERE
+//  Returns: A char containing PUT TEXT HERE
+//  Side Effect: PUT TEXT HERE
+//
 char itemGetId (const Item& item);
+
+//
+//  itemIsInInventory
+//
+//  Purpose: PUT TEXT HERE
+//  Parameter(s):
+//    <1> item: 
+//  Precondition(s): PUT TEXT HERE
+//  Returns: A boolean 
+//  Side Effect: PUT TEXT HERE
+//
 bool itemIsInInventory (const Item& item);
+
+//
+//  itemIsAtLocation
+//
+//  Purpose: PUT TEXT HERE
+//  Parameter(s):
+//    <1> item: 
+//    <2> row:
+//    <3> column:
+//  Precondition(s): PUT TEXT HERE
+//  Returns: A boolean detailing
+//  Side Effect: PUT TEXT HERE
+//
 bool itemIsAtLocation (const Item& item,
                        int row, int column);
+
+//
+//  itemGetPlayerPoints
+//
+//  Purpose: PUT TEXT HERE
+//  Parameter(s):
+//    <1> item: 
+//  Precondition(s): PUT TEXT HERE
+//  Returns: An int 
+//  Side Effect: PUT TEXT HERE
+//                     
 int itemGetPlayerPoints (const Item& item);
+
+//
+//  itemPrintDescription
+//
+//  Purpose: PUT TEXT HERE
+//  Parameter(s):
+//    <1> item: 
+//  Precondition(s): PUT TEXT HERE
+//  Returns: N/A
+//  Side Effect: PUT TEXT HERE
+//
 void itemPrintDescription (const Item& item);
+
+//
+//  itemReset
+//
+//  Purpose: PUT TEXT HERE
+//  Parameter(s):
+//    <1> item: 
+//  Precondition(s): PUT TEXT HERE
+//  Returns: N/A
+//  Side Effect: PUT TEXT HERE
+//
 void itemReset (Item& item);
+
+//
+//  itemMoveToInventory
+//
+//  Purpose: PUT TEXT HERE
+//  Parameter(s):
+//    <1> item: 
+//  Precondition(s): PUT TEXT HERE
+//  Returns: N/A
+//  Side Effect: PUT TEXT HERE
+//
 void itemMoveToInventory (Item& item);
-void itemMoveToLocation (Item& item,
+
+//
+//  itemMoveLocation
+//
+//  Purpose: PUT TEXT HERE
+//  Parameter(s):
+//    <1> item: 
+//    <2> row:
+//    <3> column:
+//  Precondition(s): PUT TEXT HERE
+//  Returns: N/A
+//  Side Effect: PUT TEXT HERE
+//
+void itemMoveToLocation (Item& item, int row, int column);
