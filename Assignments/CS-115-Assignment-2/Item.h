8b410660a66b9d72918f313dd51ac39e9e691dfe
Owen Monus <76464774+owenm2034@users.noreply.github.com>
2022-09-21 16:36:50 -0600

    Updated File Headers

--- Assignments/CS-115-Assignment-2/Item.h
+++ Assignments/CS-115-Assignment-2/Item.h
@@ -1,10 +1,8 @@
-/*
-*   Author: Owen Monus
-*   Student ID: 200482797   
-*   Date: September 21st 2022
-*   Class: CS115 _ University of Regina
-*   Purpose: Provide prototypes and constant declarations for Item
-*/
+//Author: Owen Monus
+//Student ID: 200482797   
+//Date: September 21st 2022
+//Class: CS115 _ University of Regina
+//Purpose: Provide prototypes and constant declarations for Item
 
 #pragma once
 #include <string>
